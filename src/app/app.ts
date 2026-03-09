7ff2c5832e4d6a856f7be0bd5d8c2d524aa32647
Colin Bares <colinedwinbares@gmail.com>
2026-03-09 02:42:24 -0500

    Create dice roll component and add to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { PizzaToppings } from './pizza-toppings/pizza-toppings';
+import { CbaresDiceRoll } from './cbares-dice-roll/cbares-dice-roll';
 
 @Component({
   selector: 'app-root',
-  imports: [PizzaToppings],
+  imports: [
+    PizzaToppings,
+    CbaresDiceRoll
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/cbares-dice-roll/cbares-dice-roll.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cbares-dice-roll',
+  imports: [],
+  templateUrl: './cbares-dice-roll.html',
+  styleUrl: './cbares-dice-roll.css',
+})
+export class CbaresDiceRoll {
+
+}
