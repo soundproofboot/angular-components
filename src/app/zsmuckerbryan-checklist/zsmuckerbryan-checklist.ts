8b4251679299a8fd184e295c39e8374df9a64298
Zac-Smucker-Bryan <zacary.bryan@gmail.com>
2026-03-15 15:19:12 -0500

    Wired up checkbox component
    All in a card, added checkbox input - imported in proper places

--- src/app/zsmuckerbryan-checklist/zsmuckerbryan-checklist.ts
+++ src/app/zsmuckerbryan-checklist/zsmuckerbryan-checklist.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-zsmuckerbryan-checklist',
-  imports: [],
+  imports: [
+    MatCardModule
+  ],
   templateUrl: './zsmuckerbryan-checklist.html',
   styleUrl: './zsmuckerbryan-checklist.css',
 })
